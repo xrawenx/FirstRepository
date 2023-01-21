1d83b97c32b54febcfadc5eb77e0b4ebd04f679a
xrawenx <xxnemisisxx@outlook.com>
2023-01-21 16:32:40 +0100

    slimer flatten head

--- Assets/Scripts/Enemy/Slimer.cs
+++ Assets/Scripts/Enemy/Slimer.cs
@@ -2,27 +2,35 @@ using UnityEngine;
 
 public class Slimer : MonoBehaviour
 {
-    public sprite slimer;
+    public Sprite flatSprite;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+
             if (collision.transform.DotTest(transform, Vector2.down)) {
                 Flatten();
-            }
-     
-
+            } 
+            
         }
     }
 
+    
     private void Flatten()
     {
         GetComponent<Collider2D>().enabled = false;
         GetComponent<EntityMovement>().enabled = false;
         GetComponent<AnimatedSprite>().enabled = false;
-        //GetComponent<SpriteRenderer>().sprite = slimer2;
+        GetComponent<SpriteRenderer>().sprite = flatSprite;
         Destroy(gameObject, 0.5f);
+    }
+
+    private void Hit()
+    {
+        GetComponent<AnimatedSprite>().enabled = false;
+        GetComponent<DeathAnimation>().enabled = true;
+        Destroy(gameObject, 3f);
     }
 
 }
