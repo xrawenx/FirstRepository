b82f983e0ed0d58df34d73b29371b488fd609217
xrawenx <xxnemisisxx@outlook.com>
2023-01-22 12:37:48 +0100

    Updated player startpoint

--- Assets/Scripts/Enemy/EntityMovement.cs
+++ Assets/Scripts/Enemy/EntityMovement.cs
@@ -1,54 +1,66 @@
+using UnityEditor;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class EntityMovement : MonoBehaviour
 {
-   public float speed = 1f;
-   public Vector2 direction = Vector2.left;
+    public float speed = 1f;
+    public Vector2 direction = Vector2.left;
 
-   private new Rigidbody2D rigidbody;
-   private Vector2 velocity;
+    private new Rigidbody2D rigidbody;
+    private Vector2 velocity;
 
-   private void Awake()
-   {
-	   rigidbody = GetComponent<Rigidbody2D>();
-	   enabled = false;
-   }
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        enabled = false;
+    }
 
-   private void OnBecameVisible()
-   {
-	   enabled = true;
-   }
+    private void OnBecameVisible()
+    {
+        #if UNITY_EDITOR
+        enabled = !EditorApplication.isPaused;
+        #else
+        enabled = true;
+        #endif
+    }
 
-   private void OnBecameInvisible()
-   {
-	   enabled = false;
-   }
+    private void OnBecameInvisible()
+    {
+        enabled = false;
+    }
 
-   private void OnEnable()
-   {
-	   rigidbody.WakeUp();
-   }
+    private void OnEnable()
+    {
+        rigidbody.WakeUp();
+    }
 
-   private void OnDisable()
-   {
-	   rigidbody.velocity =	Vector2.zero;
-	   rigidbody.Sleep();
-   }
+    private void OnDisable()
+    {
+        rigidbody.velocity = Vector2.zero;
+        rigidbody.Sleep();
+    }
 
-   private void FixedUpdate()
-   {
-	   velocity.x = direction.x * speed;
-	   velocity.y += Physics2D.gravity.y * Time.fixedDeltaTime;
+    private void FixedUpdate()
+    {
+        velocity.x = direction.x * speed;
+        velocity.y += Physics2D.gravity.y * Time.fixedDeltaTime;
 
-	   rigidbody.MovePosition(rigidbody.position + velocity * Time.fixedDeltaTime);
+        rigidbody.MovePosition(rigidbody.position + velocity * Time.fixedDeltaTime);
 
-	   if (rigidbody.Raycast(direction)) {
-		   direction = -direction;
-	   }
+        if (rigidbody.Raycast(direction)) {
+            direction = -direction;
+        }
 
-	   if (rigidbody.Raycast(Vector2.down)) {
-		   velocity.y = Mathf.Max(velocity.y, 0f);
-	   }
-   }
+        if (rigidbody.Raycast(Vector2.down)) {
+            velocity.y = Mathf.Max(velocity.y, 0f);
+        }
+
+        if (direction.x > 0f) {
+            transform.localEulerAngles = new Vector3(0f, 180f, 0f);
+        } else if (direction.x < 0f) {
+            transform.localEulerAngles = Vector3.zero;
+        }
+    }
 
 }
