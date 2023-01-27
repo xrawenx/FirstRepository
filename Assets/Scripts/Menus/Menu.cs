ac60ece4170759d999f96aad98fe247933156bf1
xrawenx <xxnemisisxx@outlook.com>
2023-01-27 13:46:19 +0100

    Fixing menus

--- Assets/Scripts/Menus/Menu.cs
+++ Assets/Scripts/Menus/Menu.cs
@@ -12,7 +12,13 @@ public class Menu : MonoBehaviour
 
 	public Slider[] volumeSliders;
 	public Toggle[] resolutionToggles;
+	public Toggle[] fullscreenToggle;
 	public int[] screenWidths;
+	int activeScreenResIndex;
+
+	void Start() {
+
+	}
 
    public void Play() {
 	   SceneManager.LoadScene("1-1");
@@ -48,27 +54,41 @@ public class Menu : MonoBehaviour
 
    public void SetScreenResolution(int i)
    {
-
+	   if (resolutionToggles [i].isOn) {
+		   activeScreenResIndex = i;
+		   float aspectRatio = 16 / 9f;
+		   Screen.SetResolution(screenWidths [i], (int)(screenWidths [i] / aspectRatio), false);
+		   PlayerPrefs.SetInt ("screen res index", activeScreenResIndex);
+	   }
    }
 
-   public void SetFullscreen(bool isFullscreen)
-   {
+   public void SetFullscreen(bool isFullscreen) {
+		for (int i = 0; i < resolutionToggles.Length; i++) {
+			resolutionToggles [i].interactable = !isFullscreen;
+		}
 
+		if (isFullscreen) {
+			Resolution[] allResolutions = Screen.resolutions;
+			Resolution maxResolution = allResolutions [allResolutions.Length - 1];
+			Screen.SetResolution (maxResolution.width, maxResolution.height, true);
+		} else {
+			SetScreenResolution (activeScreenResIndex);
+		}
    }
 
    public void SetMasterVolume(float value)
    {
-
+	   AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Master);
    }
 
    public void SetMusicVolume(float value)
    {
-
+	   AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Music);
    }
 
    public void SetSfxVolume(float value)
    {
-
+	   AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Sfx);
    }
 
 
